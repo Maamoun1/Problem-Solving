a43e80a5b5c8fc5b97c6b2239591c8fa5a9cc628
Mamoun ibrahim <Mamoun_mady@yahoo.com>
2024-07-31 15:34:38 +0300

    Added 5 problems

---
+++ El Mamoun/Lv 1/Problem 7.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Problem 7: Write a program to ask the user to enter: • Number Then Print the “Half of the <Number> is <???>”
+*/
+
+namespace Problem_7
+{
+    internal class Program
+    {
+
+        static int ReadNumber()
+        {
+            int Number = 0;
+            Console.Write("Please Enter a Number?: ");
+            Number = Convert.ToInt32(Console.ReadLine());
+
+            return Number;
+        }
+
+        static float HalfOfNumber(float Number)
+        {
+
+            return (float) (Number / 2);
+        }
+
+        static void PrintResult(int Number)
+        {
+            Console.WriteLine("Half of Number " + Number + " is " + HalfOfNumber(Number));
+        }
+
+        static void Main(string[] args)
+        {
+            PrintResult(ReadNumber());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ El Mamoun/Lv 1/Problem 9.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Problem 9:  Write a program to ask the user to enter:• Number1, Number2, Number3
+*/
+
+namespace Problem_9
+{
+    internal class Program
+    {
+        struct stNumbers
+        {
+            public int Number1;
+            public int Number2;
+            public int Number3;
+        }
+        static stNumbers ReadNumbers()
+        {
+            stNumbers Numbers;
+
+            Console.Write("Please Enter Number1?: ");
+            Numbers.Number1 =Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please Enter Number2?: ");
+            Numbers.Number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please Enter Number3?: ");
+            Numbers.Number3 = Convert.ToInt32(Console.ReadLine());
+
+            return Numbers;
+        }
+        static int SumOf3Numbers(stNumbers Numbers)
+        {
+            return Numbers.Number1 + Numbers.Number2 + Numbers.Number3;
+        }
+        static void PrintResult(stNumbers Numbers)
+        {
+            Console.WriteLine("Sum of 3 Numbers is: " + SumOf3Numbers(Numbers));
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            PrintResult(ReadNumbers());
+
+            Console.ReadKey();
+        }
+    }
+}
