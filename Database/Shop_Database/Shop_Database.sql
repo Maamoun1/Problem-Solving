260d22e3cf644043ac75100064d109125b5085fc
Mamoun ibrahim <Mamoun_mady@yahoo.com>
2024-08-04 17:56:17 +0300

    Update quries

---
+++ Database/Shop_Database/Shop_Database.sql
@@ -0,0 +1,27 @@
+
+USE Shop_Database;
+
+-- Get all Customers
+SELECT * FROM Customers
+
+-- Get all Orders
+SELECT * FROM Orders
+
+SELECT Customers.CustomerID, Customers.Name, Orders.amount FROM Customers INNER JOIN Orders
+ON Customers.CustomerID = Orders.CustomerID
+
+Where Orders.Amount > 500;
+
+--Left Join and Left Outer Join are the same. 
+ --Left Join: gets all data from table customers and only matched data from table orders 
+SELECT Customers.CustomerID, Customers.Name, Orders.Amount 
+FROM Customers Left JOIN Orders  ON Customers.CustomerID = Orders.CustomerID; 
+ 
+SELECT Customers.CustomerID, Customers.Name, Orders.Amount 
+FROM Customers Left Outer JOIN Orders  ON Customers.CustomerID = Orders.CustomerID; 
+
+
+
+
+
+
