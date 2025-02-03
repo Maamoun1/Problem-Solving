931a2cb5a7ae99e00f59dc493894791715737afa
Mamoun ibrahim <Mamoun_mady@yahoo.com>
2025-02-03 12:44:53 +0200

    Added Folder C# Advanced

---
+++ C#/Advanced/06 - Using Statement in C#/Using Statement/Program.cs
@@ -0,0 +1,18 @@
+﻿using Timo = System.Console;
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // using Koko alias instead of System.Console
+            Timo.WriteLine("Hello World!");
+            Timo.ReadKey();
+        }
+
+
+    }
+}
